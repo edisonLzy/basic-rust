f996b0f936cc207fdccfa12215a64034e4f32201
evanzyli <45064281+edisonLzy@users.noreply.github.com>
2022-02-16 22:38:45 +0800

    feat(切片): 了解切片

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
-fn main() {}
-
-fn show_str() -> &String {
-    let s = String::from("Hello");
-    &s // &s在函数结束之后指向了已经被销毁的内存
+fn main() {
+    let mut s = String::from("Hello");
+    let s1 = &s[1..2];
+    let s2 = "string";
+    // let i = first_word(&s);
+    println!("{}", s1)
 }
+
+// fn first_word(s: &String) -> usize {
+//     let bytes = s.as_bytes();// 将String转换字节数组
+//     for(i, &item) in bytes.iter().enumerate() {
+//        if item == b'' {
+//            return i;
+//        }
+//     }
+//     s.len()
+// }
