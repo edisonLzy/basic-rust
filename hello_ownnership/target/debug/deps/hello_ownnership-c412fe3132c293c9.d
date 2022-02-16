6f6273f15c66e3ac9afbb6ad37a1a7fca45e0dce
evanzyli <45064281+edisonLzy@users.noreply.github.com>
2022-02-16 21:43:38 +0800

    feat: add gitignore

--- hello_ownnership/target/debug/deps/hello_ownnership-c412fe3132c293c9.d
+++
@@ -1,5 +0,0 @@
-/Users/zhiyu/Desktop/web/basic-rust/hello_ownnership/target/debug/deps/hello_ownnership-c412fe3132c293c9: src/main.rs
-
-/Users/zhiyu/Desktop/web/basic-rust/hello_ownnership/target/debug/deps/hello_ownnership-c412fe3132c293c9.d: src/main.rs
-
-src/main.rs:
--- hello_ownnership/target/debug/hello_ownnership.d
+++
@@ -1,1 +0,0 @@
-/Users/zhiyu/Desktop/web/basic-rust/hello_ownnership/target/debug/hello_ownnership: /Users/zhiyu/Desktop/web/basic-rust/hello_ownnership/src/main.rs
