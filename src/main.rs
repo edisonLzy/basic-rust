49d6147ef0af06c7b09a5ff2aeafe6c22f4b4b79
evanzyli <45064281+edisonLzy@users.noreply.github.com>
2022-02-17 21:31:13 +0800

    feat(struct): 结构体基础 & 求面积demo

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,39 @@
+use std::io;
+
+#[derive(Debug)] // 添加注解 用于打印结构体实例
+struct Rectangle {
+    width: i32,
+    height: i32,
+    filled: bool,
+}
 fn main() {
-    let mut s = String::from("Hello");
-    let s1 = &s[1..2];
-    let s2 = "string";
-    // let i = first_word(&s);
-    println!("{}", s1)
+    let mut rec = Rectangle {
+        width: 0,
+        height: 0,
+        filled: true,
+    };
+    let width = get_input("please input width");
+    let height = get_input("please input height");
+    rec = Rectangle {
+        width,
+        height,
+        ..rec // 这里并不会覆盖已经显示给定的字段值
+    };
+    let result = area(&rec);
+    println!("{:#?}", rec); // 换行打印
+    println!("{:?}", rec); // 不换行打印
+    println!("{} * {} = {}", width, height, result);
 }
 
-// fn first_word(s: &String) -> usize {
-//     let bytes = s.as_bytes();// 将String转换字节数组
-//     for(i, &item) in bytes.iter().enumerate() {
-//        if item == b'' {
-//            return i;
-//        }
-//     }
-//     s.len()
-// }
+fn get_input(tip: &str) -> i32 {
+    println!("{}", tip);
+    let mut value = String::new();
+    io::stdin()
+        .read_line(&mut value)
+        .expect("fail to read the line ");
+    let value: i32 = value.trim().parse().expect("parse fail");
+    return value;
+}
+fn area(rec: &Rectangle) -> i32 {
+    rec.height * rec.width
+}
